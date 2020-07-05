ca2bc82173667dc377fd9d44cee2130abeee26a1
imhilla <hillaryodhiambo282@gmail.com>
2020-07-05 17:47:59 +0300

    edit form and add edit and update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,18 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find params[:id]
+	end
+
+	def update
+		@user = User.find(params[:id]).update user_params
+	end
+
 	private
 
 	def user_params
 		params.require(:user).permit(:username,:email,:password)
 	end
-
+	
 end
