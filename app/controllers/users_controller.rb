d616de9505b899ca63e73d1627329081def58304
ozovalihasan <ozovalihasan@gmail.com>
2020-07-06 11:37:31 +0300

    Fix errors found by rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,38 @@
 class UsersController < ApplicationController
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		# @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-		@user = User.new(user_params)
-	
-		if @user.save
-			flash[:info] = ["The user is saved successfully."]
-		else
-			flash[:info] = @user.errors.full_messages
-		end
-		redirect_to new_user_path
-	end
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
-	def edit
-		@user = User.find params[:id]
-	end
+    flash[:info] = if @user.save
+                     ['The user is saved successfully.']
+                   else
+                     @user.errors.full_messages
+                   end
+    redirect_to new_user_path
+  end
 
-	def update
-		user = User.find(params[:id])
-		if user.update(user_params)
-			flash[:info] = ["The user is edited successfully."]
-			@user = User.find(params[:id]).update user_params
-		else
-			flash[:info] = user.errors.full_messages
-		end
-		redirect_to edit_user_path(user)
+  def edit
+    @user = User.find params[:id]
+  end
 
-	end
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      flash[:info] = ['The user is edited successfully.']
+      @user = User.find(params[:id]).update user_params
+    else
+      flash[:info] = user.errors.full_messages
+    end
+    redirect_to edit_user_path(user)
+  end
 
-	private
+  private
 
-	def user_params
-		params.require(:user).permit(:username,:email,:password)
-	end
-	
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
